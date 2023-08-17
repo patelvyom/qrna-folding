2a411a6a058968a3a0017ed902b19098ef712114
Vyom Patel <vyom.patel@usask.ca>
2023-08-16 19:10:59 -0600

    add normal stem model

--- qrna_folding/preprocessor.py
+++ qrna_folding/preprocessor.py
@@ -8,6 +8,15 @@ class BasicPreProcessor(object):
     adj_matrix: np.ndarray = None
     rna_length: int = 0
     potential_stems: list[tuple[int, int, int]] = []
+    valid_bonds = {
+        "AU": 2,
+        "UA": 2,
+        "GC": 3,
+        "CG": 3,
+        "GU": 2,
+        "UG": 2,
+    }  # Values represent no. of H-bonds
+    largest_stem_length: int = 0
 
     def __init__(self, rna: str, **kwargs):
         self.rna_sequence = rna
@@ -21,24 +30,19 @@ class BasicPreProcessor(object):
         return f"BasePreProcessor(rna_sequence={self.rna_sequence})"
 
     def compute_adjacency_matrix(self, **kwargs):
-        valid = {
-            "AU": 2,
-            "UA": 2,
-            "GC": 3,
-            "CG": 3,
-            "GU": 2,
-            "UG": 2,
-        }  # Values represent no. of H-bonds
         for i in range(self.rna_length):
             for j in range(i + 1, self.rna_length):
-                if x := valid.get(self.rna_sequence[i] + self.rna_sequence[j]):
+                if x := self.valid_bonds.get(
+                    self.rna_sequence[i] + self.rna_sequence[j]
+                ):
                     self.adj_matrix[i, j] = self.adj_matrix[j, i] = x
 
     def get_adjacency_matrix(self):
         return self.adj_matrix
 
     def are_stems_overlapping(self, stem1: tuple, stem2: tuple) -> bool:
-        """Check if two stems are overlapping. Refer: https://github.com/JuanGiraldo0212/Qhack-qnyble/blob/main/qrna_folding_qaoa.ipynb
+        """Check if two stems are overlapping. Refer:
+        https://github.com/JuanGiraldo0212/Qhack-qnyble/blob/main/qrna_folding_qaoa.ipynb
         TODO: Give explicit example.
         """
         stem1_span1 = set(range(stem1[0], stem1[0] + stem1[2]))
@@ -56,7 +60,9 @@ class BasicPreProcessor(object):
         return False
 
     def are_stems_pseudoknotted(self, stem1: tuple, stem2: tuple) -> bool:
-        """Check if two stems are pseudoknotted. Refer: Fox et al. https://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1010032"""
+        """Check if two stems are pseudoknotted. Refer: Fox et al.
+        https://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1010032
+        """
 
         assert not self.are_stems_overlapping(
             stem1, stem2
@@ -71,11 +77,44 @@ class BasicPreProcessor(object):
             return True
         return False
 
-    def compute_potential_stems(self, **kwargs):
+    def compute_potential_stems(self):
         raise NotImplementedError
 
     def get_potential_stems(self):
         return self.potential_stems
 
+    def get_largest_stem_length(self):
+        return self.largest_stem_length
+
     def process(self, **kwargs):
         raise NotImplementedError
+
+
+class NormalStemLengthPreProcessor(BasicPreProcessor):
+    """
+    Preprocessor where stem length model is the basic (BP count) model.
+    """
+
+    def __init__(self, rna: str, **kwargs):
+        super().__init__(rna, **kwargs)
+
+    def compute_potential_stems(self, min_stem_length: int = 3):
+        matrix = np.triu(self.adj_matrix)  # Upper triangular matrix because of symmetry
+        for i in range(self.rna_length):
+            for j in range(i + 1, self.rna_length):
+                if matrix[i, j] > 0:
+                    i_temp, j_temp = i, j
+                    stem_length = 0
+                    while matrix[i_temp, j_temp] > 0:
+                        stem_length += 1
+                        i_temp += 1
+                        j_temp -= 1
+                        if stem_length >= min_stem_length:
+                            stem = (i + 1, j + 1, stem_length)
+                            self.potential_stems.append(stem)
+                    if stem_length > self.largest_stem_length:
+                        self.largest_stem_length = stem_length
+
+    def process(self, min_stem_length: int = 3):
+        self.compute_adjacency_matrix()
+        self.compute_potential_stems(min_stem_length=min_stem_length)
